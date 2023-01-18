121c77cfbf29dd576acd2ec6f211191fcf923b2e
kz-sik <115482594+kz-sik@users.noreply.github.com>
2023-01-18 16:11:01 +0000

    add genre maker index edit

--- app/controllers/public/makers_controller.rb
+++ app/controllers/public/makers_controller.rb
@@ -1,13 +1,28 @@
 class Public::MakersController < ApplicationController
   def index
+    @maker = Maker.new
+    @makers = Maker.all
   end
 
   def create
+    @maker = Maker.new(maker_params)
+    @maker.save
+    redirect_to makers_path
   end
 
   def edit
+    @maker = Maker.find(params[:id])
   end
 
   def update
+    @maker = Maker.find(params[:id])
+    @maker.update(maker_params)
+    redirect_to makers_path
+  end
+
+  private
+
+  def maker_params
+    params.require(:maker).permit(:name)
   end
 end
