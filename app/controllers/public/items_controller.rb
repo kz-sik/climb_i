696d91f5a340955f2ba90abcb6b7d80dc0f1b96f
kz-sik <115482594+kz-sik@users.noreply.github.com>
2023-01-19 14:14:11 +0000

    edit item new

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,19 +1,27 @@
 class Public::ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+  end
+
   def index
   end
 
   def show
   end
 
-  def new
-  end
-
-  def create
-  end
-
   def edit
   end
 
   def update
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:genre_id, :maker_id, :name, :introduction, :image)
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ApplicationRecord
+  has_many :itemtags, dependent: :destroy
+  has_many :items, through: :itemtags
 end
