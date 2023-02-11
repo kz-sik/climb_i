538f07a45401451c6f1f57a17fcbcebe3a6718ac
kz-sik <115482594+kz-sik@users.noreply.github.com>
2023-02-11 15:05:24 +0000

    add admin user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,25 @@
 class Admin::UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to admin_user_path(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email, :encrypted_password, :height, :sex, :is_deleted)
   end
 end
